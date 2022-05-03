bcfa154b252c27503aefbc029816cc32df049791
dharrmik22 <85289713+dharrmik22@users.noreply.github.com>
2022-05-03 19:25:25 -0400

    Add files via upload

---
+++ first.py
@@ -0,0 +1,121 @@
+# my_integer_variable = 1000
+# my_float_variable = 3.5
+# my_complex_variable = 1 + 2j
+# 
+# print(type(my_complex_variable))
+# print(type(my_float_variable))
+# print(type(my_integer_variable))
+# 
+# string = "This is a string variabale"
+# all_words = string.split()
+# first_word= all_words[0]
+# print(first_word)
+# 
+# 
+# print(all_words)
+# 
+# power = 2**3
+# print(power)
+
+# r = 20%7
+# print(r)
+
+# 
+# x = "Python is "
+# y = "awesome"
+# z =  x + y
+# print(z)
+# 
+# 
+# s1 = 'String'
+# s2 = 'Concatenation'
+# s3 = ''.join([s1, s2])
+# print(s3)
+# 
+# 
+# 
+# 
+# s1, s2, s3 = 'Python', 'String', 'Concatenation'
+# s = '%s %s %s' % (s1, s2, s3)
+# print(s)
+# 
+# 
+# 
+# 
+# s1, s2, s3 = 'Python', 'String', 'Concatenation'
+# s = '{} {} {}'.format(s1, s2, s3)
+# print(s)
+# 
+# 
+# s1, s2, s3 = 'Python', 'String', 'Concatenation'
+# s = f'{s1} {s2} {s3}'
+# print(s)
+
+
+first_input = float(input("Enter the first number: "))
+second_input = float(input("Enter the second number: "))
+
+addition = first_input + second_input
+
+substraction = first_input - second_input
+
+multiplication = first_input * second_input
+
+division = first_input / second_input
+
+reminder = first_input % second_input
+
+power = first_input ** second_input
+
+
+
+print("Addition: ",addition)
+print("substraction: ",substraction)
+print("multiplication: ",multiplication)
+print("division: ",division)
+print("reminder: ",reminder)
+print("power: ",power)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
